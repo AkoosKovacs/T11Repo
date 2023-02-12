4946fdcb1b3c741f84c10e668ed32e4d0d228e47
AkoosKovacs <kovacsakoos@gmail.com>
2023-02-12 20:31:29 +0100

    szorgalmi

---
+++ 2023_02.09_Szorgalmi/fifaStatisztikak.js
@@ -0,0 +1,76 @@
+<script>
+const csapatAdat = [
+"Anglia;4;0;1662", 
+"Argentína;10;0;1614", 
+"Belgium;1;0;1752", 
+"Brazília;3;-1;1719", 
+"Chile;17;-3;1576", 
+"Dánia;14;-1;1584", 
+"Franciaország;2;1;1725", 
+"Hollandia;13;3;1586", 
+"Horvátország;8;-1;1625", 
+"Kolumbia;9;-1;1622", 
+"Mexikó;12;0;1603", 
+"Németország;16;-1;1580", 
+"Olaszország;15;1;1583", 
+"Peru;19;0;1551", 
+"Portugália;5;1;1643", 
+"Spanyolország;7;2;1631", 
+"Svájc;11;0;1604", 
+"Svédország;18;0;1560", 
+"Szenegál;20;0;1546", 
+"Uruguay;6;-1;1639"
+];
+
+//Objektum típusú tömb készítése
+function ObjektumFeltoto(feltoltendoElem){
+	const beolvasottAdat=[];
+    for(let i=0;i<feltoltendoElem.length;i++){
+    	let objektum={};
+        let daraboltAdatSor=feltoltendoElem[i].split(';');
+        objektum.nev=daraboltAdatSor[0];
+        objektum.helyezes=daraboltAdatSor[1];
+        objektum.valtozas=daraboltAdatSor[2];
+        objektum.pontszam=daraboltAdatSor[3];
+        beolvasottAdat.push(objektum);
+    }
+    return beolvasottAdat;
+}
+const fifaAdatok=ObjektumFeltoto(csapatAdat);
+
+
+/*----------1.Feladat: Adja meg aktuálisan hány csapat szerepel a ranglistán------------*/
+document.write("1.Feladat: Adja meg aktuálisan hány csapat szerepel a ranglistán")
+document.write("<br>A ranglistán jelenleg "+fifaAdatok.length+"db csapat szerepel<hr>")
+
+
+/*-------------2.Feladat: Írja ki mennyi a résztvevő csapatok átlagpontszáma------------*/
+document.write("2.Feladat: Írja ki mennyi a résztvevő csapatok átlagpontszáma")
+
+function AtlagPontszamKiszamitas(adatok){
+	let pontszamokOsszege=0;
+	for(let i=0; i<adatok.length; i++){
+		pontszamokOsszege=pontszamokOsszege+Number(adatok[i].pontszam)
+	}
+	return Math.round(pontszamokOsszege/adatok.length);
+}
+
+let atlagPontszam=(AtlagPontszamKiszamitas(fifaAdatok));
+document.write("<br>A résztvevő csapatok átlagpontszáma: "+atlagPontszam+"<hr>")
+
+
+/*--3.Feladat: Listázza ki azokat a csapatokat, akik az átlagnál több pontot értek el!--*/
+document.write("3.Feladat: Listázza ki azokat a csapatokat, akik az átlagnál több pontot értek el!<br>")
+
+function AtlagFolottiPontszamosCsapatokKiirasa(adatok,atlagPontszam){
+	let atlagFolottiPontszamosCsapatok=[]
+    for(let i=0; i<adatok.length; i++){
+    	if(adatok[i].pontszam>atlagPontszam){
+        	atlagFolottiPontszamosCsapatok.push(adatok[i].nev)
+        }
+    }
+    return atlagFolottiPontszamosCsapatok
+}
+
+document.write("<br>Az átlagpontszámnál több pontot elérő csapatok: "+AtlagFolottiPontszamosCsapatokKiirasa(fifaAdatok,atlagPontszam))
+</script>
